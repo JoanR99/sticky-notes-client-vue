d04f58b036f846778cfbb329054cb9aa6ea4060c
Joan Romero <romerojoan1999@gmail.com>
2023-02-01 18:46:52 -0400

    setup project

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <div
+      class="flex justify-between px-4 items-center bg-teal-700 text-white py-2"
+    >
+      <RouterLink to="/" class="text-xl font-bold">Sticky Notes</RouterLink>
+      <nav class="flex gap-2">
+        <RouterLink to="/login">Login</RouterLink>
+        <RouterLink to="/register">Sign Up</RouterLink>
+      </nav>
+    </div>
+  </header>
+</template>
