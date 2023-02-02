5069499c52d9da145463c40e9bd7b91d1d6df5df
Joan Romero <romerojoan1999@gmail.com>
2023-02-02 15:40:06 -0400

    add NoteCard component

---
+++ src/components/NoteCard.vue
@@ -0,0 +1,113 @@
+<template>
+  <article
+    class="w-full max-w-md overflow-hidden rounded-2xl p-6 text-left align-middle shadow-xl"
+    :class="getColor(props.note.color)"
+    @click="openModal"
+  >
+    <h2 class="text-lg font-medium leading-6 text-gray-900">
+      {{ props.note.title }}
+    </h2>
+    <p>{{ props.note.content }}</p>
+  </article>
+
+  <TransitionRoot v-if="isOpen" appear :show="showAnimation" as="template">
+    <Dialog as="div" @close="closeModal" class="relative z-10">
+      <TransitionChild
+        as="template"
+        enter="duration-300 ease-out"
+        enter-from="opacity-0"
+        enter-to="opacity-100"
+        leave="duration-200 ease-in"
+        leave-from="opacity-100"
+        leave-to="opacity-0"
+      >
+        <div class="fixed inset-0 bg-black bg-opacity-25" />
+      </TransitionChild>
+
+      <div class="fixed inset-0 overflow-y-auto">
+        <div
+          class="flex min-h-full items-center justify-center p-4 text-center"
+        >
+          <TransitionChild
+            as="template"
+            enter="duration-300 ease-out"
+            enter-from="opacity-0 scale-95"
+            enter-to="opacity-100 scale-100"
+            leave="duration-200 ease-in"
+            leave-from="opacity-100 scale-100"
+            leave-to="opacity-0 scale-95"
+          >
+            <DialogPanel
+              class="w-full max-w-md transform overflow-hidden rounded-2xl p-6 text-left align-middle shadow-xl transition-all"
+              :class="getColor(props.note.color)"
+            >
+              <DialogTitle
+                as="h3"
+                class="text-lg font-medium leading-6 text-gray-900"
+              >
+                {{ props.note.title }}
+              </DialogTitle>
+              <div class="mt-2">
+                <p class="text-sm text-gray-500">
+                  {{ props.note.content }}
+                </p>
+              </div>
+
+              <div class="mt-4">
+                <button
+                  type="button"
+                  class="inline-flex justify-center rounded-md border border-transparent bg-red-200 px-4 py-2 text-sm font-medium text-gray-900 hover:bg-red-300 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2"
+                  @click="closeModal"
+                >
+                  Close
+                </button>
+              </div>
+            </DialogPanel>
+          </TransitionChild>
+        </div>
+      </div>
+    </Dialog>
+  </TransitionRoot>
+</template>
+
+<script setup lang="ts">
+import { ref, defineProps } from "vue";
+import {
+  TransitionRoot,
+  TransitionChild,
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+} from "@headlessui/vue";
+
+type Note = {
+  title: string;
+  content: string;
+  color: string;
+};
+
+const props = defineProps<{ note: Note }>();
+
+function getColor(color: string) {
+  if (color === "blue") {
+    return "bg-blue-200";
+  }
+
+  return "bg-yellow-200";
+}
+
+const isOpen = ref(false);
+const showAnimation = ref(false);
+
+function closeModal() {
+  showAnimation.value = false;
+
+  setTimeout(() => {
+    isOpen.value = false;
+  }, 500);
+}
+function openModal() {
+  isOpen.value = true;
+  showAnimation.value = true;
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import NoteCard from "@/components/NoteCard.vue";
+
+const notes = [
+  {
+    id: 1,
+    title: "Joan",
+    content: "Recuerda",
+    color: "blue",
+  },
+  {
+    id: 2,
+    title: "Joan",
+    content: "Recuerda",
+    color: "blue",
+  },
+];
+</script>
 
 <template>
-  <main></main>
+  <main>
+    <div class="grid grid-cols-4 gap-4 p-8 px-40">
+      <NoteCard v-for="note in notes" v-bind:key="note.id" :note="note" />
+    </div>
+  </main>
 </template>
