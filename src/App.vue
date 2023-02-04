e0ded910c27432ac11e9955114a0c0d673d76d01
Joan Romero <romerojoan1999@gmail.com>
2023-02-03 23:02:49 -0400

    add secondary header component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
 import MainHeader from "./components/MainHeader.vue";
+import SecondaryHeader from "./components/SecondaryHeader.vue";
 </script>
 
 <template>
   <MainHeader />
+  <SecondaryHeader />
   <RouterView />
 </template>
 
---
+++ src/components/SecondaryHeader.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useIsArchiveStore } from "../stores/isArchive";
+import { storeToRefs } from "pinia";
+const isArchiveStore = useIsArchiveStore();
+const { isArchive } = storeToRefs(isArchiveStore);
+const { toggleIsArchive } = isArchiveStore;
+</script>
+
+<template>
+  <header class="fixed top-10 w-full">
+    <div
+      class="flex justify-between px-4 items-center bg-gray-400 text-black py-2"
+    >
+      <button @click="toggleIsArchive">
+        {{ isArchive ? "Unarchive Notes" : "Archived Notes" }}
+      </button>
+    </div>
+  </header>
+</template>
