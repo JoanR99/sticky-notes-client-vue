224946c096a9d7e9977457baaf6293fbd9f00a10
Joan Romero <romerojoan1999@gmail.com>
2023-02-03 22:56:06 -0400

    add types to NoteCard component

--- src/components/NoteCard.vue
+++ src/components/NoteCard.vue
@@ -79,21 +79,33 @@ import {
   DialogPanel,
   DialogTitle,
 } from "@headlessui/vue";
-
-type Note = {
-  title: string;
-  content: string;
-  color: string;
-};
+import type { Note, Color } from "@/schemas/noteSchemas";
 
 const props = defineProps<{ note: Note }>();
 
-function getColor(color: string) {
-  if (color === "blue") {
-    return "bg-blue-200";
+function getColor(color: Color) {
+  switch (color) {
+    case "blue":
+      return "bg-blue-200";
+    case "green":
+      return "bg-green-200";
+    case "yellow":
+      return "bg-yellow-200";
+    case "brown":
+      return "bg-stone-400";
+    case "pink":
+      return "bg-pink-200";
+    case "red":
+      return "bg-red-200";
+    case "orange":
+      return "bg-orange-200";
+    case "purple":
+      return "bg-purple-200";
+    case "white":
+      return "bg-white";
+    case "gray":
+      return "bg-gray-200";
   }
-
-  return "bg-yellow-200";
 }
 
 const isOpen = ref(false);
